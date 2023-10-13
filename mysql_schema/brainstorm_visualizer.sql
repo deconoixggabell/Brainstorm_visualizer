42f7af3c0eb05babfa8f6ead4f5fc1b8b4f2e288
springsteenptrck <springsteenptrck@gmail.com>
2023-10-13 19:06:15 -0400

    Added "ideas" table
    Added a one-to-many relationship between users and ideas. Implemented a Main Idea, along with 5 Categories and 3 sub-categories per category.

--- mysql_schema/brainstorm_visualizer.sql
+++ mysql_schema/brainstorm_visualizer.sql
@@ -30,6 +30,43 @@ CREATE TABLE IF NOT EXISTS `brainstorm_visualizer`.`users` (
 ENGINE = InnoDB;
 
 
+-- -----------------------------------------------------
+-- Table `brainstorm_visualizer`.`ideas`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `brainstorm_visualizer`.`ideas` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `main_idea` VARCHAR(255) NULL,
+  `cat_i` VARCHAR(255) NULL,
+  `sub_c_i` VARCHAR(255) NULL,
+  `sub_c_ii` VARCHAR(255) NULL,
+  `sub_c_iii` VARCHAR(255) NULL,
+  `cat_ii` VARCHAR(255) NULL,
+  `sub_c_iv` VARCHAR(255) NULL,
+  `sub_c_v` VARCHAR(255) NULL,
+  `sub_c_vi` VARCHAR(255) NULL,
+  `cat_iii` VARCHAR(255) NULL,
+  `sub_c_vii` VARCHAR(255) NULL,
+  `sub_c_viii` VARCHAR(255) NULL,
+  `sub_c_ix` VARCHAR(255) NULL,
+  `cat_iv` VARCHAR(255) NULL,
+  `sub_c_x` VARCHAR(255) NULL,
+  `sub_c_xi` VARCHAR(255) NULL,
+  `sub_c_xii` VARCHAR(255) NULL,
+  `cat_v` VARCHAR(255) NULL,
+  `sub_c_xiii` VARCHAR(255) NULL,
+  `sub_c_xiv` VARCHAR(255) NULL,
+  `sub_c_xv` VARCHAR(255) NULL,
+  `users_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_ideas_users_idx` (`users_id` ASC) VISIBLE,
+  CONSTRAINT `fk_ideas_users`
+    FOREIGN KEY (`users_id`)
+    REFERENCES `brainstorm_visualizer`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
