058a5cf9c55378c4fc2e02f3e277985518c991b2
deconoixggabell <deconoixggabell@gmail.com>
2023-10-17 20:53:13 -0600

    merge

--- server.py
+++
@@ -1,9 +0,0 @@
-from flask_app import app
-from flask_app.controllers import users, ideas
-
-
-if __name__=="__main__":   
-    app.run(debug=True) 
-    # On line six you can change the port number.
-
-# debug needs to be set to False when deployed.
