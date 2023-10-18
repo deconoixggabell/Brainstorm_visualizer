741c741260f611d68cbf2cd72bbb6bd10d44e875
deconoixggabell <deconoixggabell@gmail.com>
2023-10-18 00:56:15 -0600

    working api test

--- flask_app/controllers/Api.py
+++ flask_app/controllers/Api.py
@@ -1,13 +1,24 @@
 from flask import *
-from flask_app import app
 import json, time
+from flask import render_template, redirect, request, session, flash 
+from flask_app import app
+from flask_app.models.idea import Idea  
+from flask_app.models.user import User
 
-@app.rounte('/', methods=['GET'])
-def api():
-    data_set = {
-        'test': 'test',
-        'message':'it worked yay',
-        'timestamp': time.time()
-    }
-    result = json.dumps(data_set)
-    return result
+
+# working code in controllers users
+
+
+# @app.route('/api')
+# def api():
+#     return render_template("visual_code.html")
+
+# @app.route('/', methods=['GET'])
+# def api():
+#     data_set = {
+#         'test': 'test',
+#         'message':'it worked yay',
+#         'timestamp': time.time()
+#     }
+#     result = json.dumps(data_set)
+#     return redirect('/api')
