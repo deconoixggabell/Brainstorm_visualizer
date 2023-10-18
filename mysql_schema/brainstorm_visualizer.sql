85e48e7a96159b8686618a583c52b0472634d3cc
Franco_H <francoh.jobs@gmail.com>
2023-10-18 17:30:54 -0400

    updated sql file for Glory, the schema file didn't work on his mac

--- mysql_schema/brainstorm_visualizer.sql
+++ mysql_schema/brainstorm_visualizer.sql
@@ -5,6 +5,9 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
 -- Schema brainstorm_visualizer
 -- -----------------------------------------------------
 DROP SCHEMA IF EXISTS `brainstorm_visualizer` ;
@@ -12,7 +15,7 @@ DROP SCHEMA IF EXISTS `brainstorm_visualizer` ;
 -- -----------------------------------------------------
 -- Schema brainstorm_visualizer
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `brainstorm_visualizer` DEFAULT CHARACTER SET utf8 ;
+CREATE SCHEMA IF NOT EXISTS `brainstorm_visualizer` DEFAULT CHARACTER SET utf8mb3 ;
 USE `brainstorm_visualizer` ;
 
 -- -----------------------------------------------------
@@ -24,10 +27,12 @@ CREATE TABLE IF NOT EXISTS `brainstorm_visualizer`.`users` (
   `last_name` VARCHAR(255) NOT NULL,
   `email` VARCHAR(255) NOT NULL,
   `password` VARCHAR(255) NOT NULL,
-  `created_at` DATETIME NOT NULL DEFAULT NOW(),
-  `updated_at` DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
@@ -35,36 +40,36 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `brainstorm_visualizer`.`ideas` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `main_idea` VARCHAR(255) NULL,
-  `cat_i` VARCHAR(255) NULL,
-  `sub_c_i` VARCHAR(255) NULL,
-  `sub_c_ii` VARCHAR(255) NULL,
-  `sub_c_iii` VARCHAR(255) NULL,
-  `cat_ii` VARCHAR(255) NULL,
-  `sub_c_iv` VARCHAR(255) NULL,
-  `sub_c_v` VARCHAR(255) NULL,
-  `sub_c_vi` VARCHAR(255) NULL,
-  `cat_iii` VARCHAR(255) NULL,
-  `sub_c_vii` VARCHAR(255) NULL,
-  `sub_c_viii` VARCHAR(255) NULL,
-  `sub_c_ix` VARCHAR(255) NULL,
-  `cat_iv` VARCHAR(255) NULL,
-  `sub_c_x` VARCHAR(255) NULL,
-  `sub_c_xi` VARCHAR(255) NULL,
-  `sub_c_xii` VARCHAR(255) NULL,
-  `cat_v` VARCHAR(255) NULL,
-  `sub_c_xiii` VARCHAR(255) NULL,
-  `sub_c_xiv` VARCHAR(255) NULL,
-  `sub_c_xv` VARCHAR(255) NULL,
+  `main_idea` VARCHAR(255) NULL DEFAULT NULL,
+  `cat_1` VARCHAR(255) NULL DEFAULT NULL,
+  `sub_c_1_1` VARCHAR(255) NULL DEFAULT NULL,
+  `sub_c_1_2` VARCHAR(255) NULL DEFAULT NULL,
+  `sub_c_1_3` VARCHAR(255) NULL DEFAULT NULL,
+  `cat_2` VARCHAR(255) NULL DEFAULT NULL,
+  `sub_c_2_1` VARCHAR(255) NULL DEFAULT NULL,
+  `sub_c_2_2` VARCHAR(255) NULL DEFAULT NULL,
+  `sub_c_2_3` VARCHAR(255) NULL DEFAULT NULL,
+  `cat_3` VARCHAR(255) NULL DEFAULT NULL,
+  `sub_c_3_1` VARCHAR(255) NULL DEFAULT NULL,
+  `sub_c_3_2` VARCHAR(255) NULL DEFAULT NULL,
+  `sub_c_3_3` VARCHAR(255) NULL DEFAULT NULL,
+  `cat_4` VARCHAR(255) NULL DEFAULT NULL,
+  `sub_c_4_1` VARCHAR(255) NULL DEFAULT NULL,
+  `sub_c_4_2` VARCHAR(255) NULL DEFAULT NULL,
+  `sub_c_4_3` VARCHAR(255) NULL DEFAULT NULL,
+  `cat_5` VARCHAR(255) NULL DEFAULT NULL,
+  `sub_c_5_1` VARCHAR(255) NULL DEFAULT NULL,
+  `sub_c_5_2` VARCHAR(255) NULL DEFAULT NULL,
+  `sub_c_5_3` VARCHAR(255) NULL DEFAULT NULL,
   `users_id` INT NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_ideas_users_idx` (`users_id` ASC) VISIBLE,
   CONSTRAINT `fk_ideas_users`
     FOREIGN KEY (`users_id`)
-    REFERENCES `brainstorm_visualizer`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `brainstorm_visualizer`.`users` (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 5
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
